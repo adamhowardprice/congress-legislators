2930094acd6aa679a02072b6665817586685d09b
Adam Price <adamprice@spotify.com>
2016-12-07 01:43:37 -0500

    WIP

--- scripts/house_state_contacts.py
+++ scripts/house_state_contacts.py
@@ -24,6 +24,7 @@ def run():
 
 	cong_to_phones = {}
 	missing = []
+	others = ['CA16', 'FL04', 'MD07', 'IL07', 'PA15', 'TN04', 'FL21', 'WI07', 'MD04', 'TX27', 'VA04', 'NE01', 'NJ11', 'NY19', 'TX01', 'GA14', 'AZ03', 'NY22', 'WA03', 'CA17', 'OH06', 'OH04', 'CA13', 'IL03', 'CA19', 'MO03', 'WA07', 'FL11', 'CA22', 'ME01', 'TX02', 'KS04', 'NY13', 'NY23', 'VA02', 'CA48', 'FL17', 'FL15', 'CA39', 'WI01', 'CA28', 'OR05', 'WI05', 'ID02', 'IN03', 'MS02', 'TX13', 'MI06', 'FL10', 'AR03', 'KY03', 'AK00', 'CA44', 'AZ01', 'CA15', 'CA41', 'FL18', 'IL13', 'IN02', 'NM01', 'OH02', 'OK01', 'TX16', 'WA06', 'FL13', 'FL19', 'NJ01', 'NC12', 'CA25', 'FL26', 'GA10', 'GA12', 'NY04', 'TX36', 'VA08', 'WI06', 'MS01', 'OH08']
 
 	for idx, moc in enumerate(y):
 		print(idx)
@@ -45,6 +46,8 @@ def run():
 		if "class" in term: del term["class"]
 
 		cong_code = "%s%02d" % (term["state"], term["district"])
+		if cong_code not in others:
+			continue
 		print(cong_code)
 
 		if "url" not in term:
@@ -52,7 +55,8 @@ def run():
 			continue
 
 		url = term["url"]
-		cache = "legislators/subdomain_house_contact/%s.html" % cong_code
+
+		cache = "legislators/subdomain_house/%s.html" % cong_code
 		try:
 			# the meta tag say it's iso-8859-1, but... names are actually in utf8...
 			body = download(url, cache, force)
@@ -62,12 +66,22 @@ def run():
 			missing.append(cong_code)
 			continue
 
-		if dom is None:
+		contact_url = url + "/contact"
+		contact_cache = "legislators/subdomain_house_contact/%s.html" % cong_code
+		try:
+			contact_body = download(contact_url, cache, force)
+			contact_dom = lxml.html.parse(io.StringIO(contact_body)).getroot()
+		except:
+			print("Error parsing: ", contact_url)
+			missing.append(cong_code)
+			continue
+
+		if dom is None or contact_dom is None:
 			missing.append(cong_code)
 			continue
 
 		phones = []
-		phone_doms = dom.cssselect("*:contains(Phone\:)")
+		phone_doms = dom.cssselect("*:contains(Office\:)")
 		for p in phone_doms:
 			phone_number_string = str(p.text_content())
 			phone_matches = re.search("(\(\d{3}\) \d{3}-\d{4}|\d{3}\.\d{3}\.\d{4}|\d{3}-\d{3}-\d{4})", phone_number_string)
@@ -81,8 +95,7 @@ def run():
 				missing.append(cong_code)
 				continue
 
-			cong_to_phones[cong_code] = phones
-
+		cong_to_phones[cong_code] = phones
 		print(phones)
 
 	# save_data(y, "legislators-current.yaml")
